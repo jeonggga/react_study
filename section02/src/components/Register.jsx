ebef6fb48629c9658b2204945603c57b217ab157
jeonggga <gyj_802@naver.com>
2025-12-30 20:50:20 +0900

    251230 리액트 공부 05 (사용자 입력 관리하기2)

--- section02/src/components/Register.jsx
+++ section02/src/components/Register.jsx
@@ -6,38 +6,72 @@ import { useState } from "react";
 // 3. 국적
 // 4. 자기소개
 
+// 비슷한 여러개의 state가 있을 경우 하나의 객체 값으로 묶어서 하나의 state로 통합해서 관리 가능
 const Register = () => {
-  const [name, setName] = useState("이름");
-  const [birth, setBirth] = useState("");
-  const [country, setCountry] = useState("");
-  const [bio, setBio] = useState("");
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  // 여러개 비슷하게 동작하는 이벤트 핸들러들을 하나의 통합된 이벤트 핸들러로 합쳐서 사용 가능
+  const onChange = (e) => {
+    console.log(e.target.name, e.target.value);
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  const onChangeBirth = (e) => {
-    setBirth(e.target.value);
-  };
+  //   const onChangeName = (e) => {
+  //     setInput({
+  //       ...input, // name 아닌 다른 값들을 변경하지 않고 그대로 유지시킴
+  //       name: e.target.value, // 사용자가 입력한 값으로 name 프로퍼티 값으로 수정됨
+  //     });
+  //   };
 
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value);
-  };
+  //   const onChangeBirth = (e) => {
+  //     setInput({
+  //       ...input,
+  //       birth: e.target.value,
+  //     });
+  //   };
 
-  const onChangeBio = (e) => {
-    setBio(e.target.value);
-  };
+  //   const onChangeCountry = (e) => {
+  //     setInput({
+  //       ...input,
+  //       country: e.target.value,
+  //     });
+  //   };
+
+  //   const onChangeBio = (e) => {
+  //     setInput({
+  //       ...input,
+  //       bio: e.target.value,
+  //     });
+  //   };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} placeholder={"이름"} />
+        <input
+          name="name"
+          value={input.name}
+          onChange={onChange}
+          placeholder={"이름"}
+        />
       </div>
       <div>
-        <input value={birth} onChange={onChangeBirth} type="date" />
+        <input
+          name="birth"
+          value={input.birth}
+          onChange={onChange}
+          type="date"
+        />
       </div>
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <select name="country" value={input.country} onChange={onChange}>
           <option value=""></option>
           <option value="kr">한국</option>
           <option value="us">미국</option>
@@ -45,7 +79,7 @@ const Register = () => {
         </select>
       </div>
       <div>
-        <textarea value={bio} onChange={onChangeBio} />
+        <textarea name="bio" value={input.bio} onChange={onChange} />
       </div>
     </div>
   );
