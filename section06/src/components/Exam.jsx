e21499945822eb2a78c5f090f3b78fdae48fb536
jeonggga <gyj_802@naver.com>
2026-01-04 21:39:47 +0900

    260104 리액트 공부 16 (useReducer 사용하기)

---
+++ section06/src/components/Exam.jsx
@@ -0,0 +1,53 @@
+import { useReducer } from "react";
+
+// reducer: 변환기
+// -> 상태를 실제로 변화시키는 변환기 역할
+function reducer(state, action) {
+  //   if (action.type === "INCREASE") {
+  //     return state + action.data;
+  //   } else if (action.type === "DECREASE") {
+  //     return state - action.data;
+  //   }
+
+  // 액션 타입이 많아질 것 같은 경우에는 if문이 아니라 switch문으로 하는 것이 일반적임
+  switch (action.type) {
+    case "INCREASE":
+      return state + action.data;
+    case "DECREASE":
+      return state - action.data;
+    default:
+      return state;
+  }
+}
+
+const Exam = () => {
+  // dispatch 발송하다, 급송하다
+  // -> 상태 변화가 있어야 한다는 사실을 알리는, 발송하는 함수
+  const [state, dispatch] = useReducer(reducer, 0);
+
+  const onClickPlus = () => {
+    // 인수: 상태가 어떻게 변화되길 원하는지
+    // -> 액션 객체
+    dispatch({
+      type: "INCREASE",
+      data: 1,
+    });
+  };
+
+  const onClickMinus = () => {
+    dispatch({
+      type: "DECREASE",
+      data: 1,
+    });
+  };
+
+  return (
+    <div>
+      <h1>{state}</h1>
+      <button onClick={onClickPlus}>+</button>
+      <button onClick={onClickMinus}>-</button>
+    </div>
+  );
+};
+
+export default Exam;
